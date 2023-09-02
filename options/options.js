202f473faf6d0b15ed08debb4870e5765b8bc685
Crowdbotics <engineering@crowdbotics.com>
2023-09-02 01:44:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "green-queen-8",
+  url: "https://green-queen-8.botics.co",
+  api: "https://green-queen-8.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
